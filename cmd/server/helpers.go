1d681df8f66d1bb0c860058b621798cc4144e6c3
Oliver Jakoubek <mail@oliverjakoubek.de>
2022-05-10 12:31:51 +0200

    Return correct HTTP status code instead of always 200

--- cmd/server/helpers.go
+++ cmd/server/helpers.go
@@ -25,7 +25,7 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	//w.WriteHeader(status)
+	w.WriteHeader(status)
 	w.Write(js)
 
 	return nil
